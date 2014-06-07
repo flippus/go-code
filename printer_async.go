df1ee803bce63742c70cd823c7c7b0a22d02ed99
Philipp Neugebauer <ppn@nexgo.de>
2014-06-07 18:01:29 +0200

    async printer added

---
+++ printer_async.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func sender(c chan int) {
+    c <- 2
+    c <- 3
+    c <- 4
+    close(c)
+}
+
+func sender2(c chan int) {
+    c <- 5
+    c <- 6
+    c <- 9
+    c <- 10
+    c <- 15
+}
+
+func printer(c chan int) {
+    for i := range c {
+        fmt.Println("message received:", i)
+    }
+}
+
+func main() {
+    c := make(chan int, 3)
+
+    go sender2(c)
+    go sender(c)
+    printer(c)
+}
